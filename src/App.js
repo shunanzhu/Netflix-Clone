51aa2375159a370d6945c53b028a05e1b67b9a00
shunanzhu <shunanzhu@gmail.com>
2022-09-03 18:31:03 -0700

    Test Commit 2

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Shunan's First React Project
         </a>
       </header>
     </div>
